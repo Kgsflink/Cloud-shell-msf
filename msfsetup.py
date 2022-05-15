1daec833783beeffe55dc823767728b6976954db
Gopal <kgopalsahani8@gmail.com>
2022-05-15 15:07:58 +0000

    last commit

---
+++ msfsetup.py
@@ -0,0 +1,20 @@
+# its made by krishan (devilboy66)
+#Its my youtube channel "https://youtube.com/channel/UCf5uga-16WMdT7ded736PDA"
+#Insta gram id krishna666 
+#teligram link https://t.me/kgsflink
+import os
+
+cmd = 'sudo apt update'
+os.system(cmd)
+cmd = 'sudo curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok-stable-linux-amd64.zip'
+os.system(cmd)
+cmd = 'sudo unzip ngrok-stable-linux-amd64.zip'
+os.system(cmd)
+#cmd = 'sudo apt install git'
+#os.system(cmd)
+cmd = 'sudo git clone https://github.com/rapid7/metasploit-framework'
+os.system(cmd)
+cmd = 'cd metasploit-framework'
+os.system(cmd)
+cmd = 'bash msfconsole'
+os.system(cmd)
